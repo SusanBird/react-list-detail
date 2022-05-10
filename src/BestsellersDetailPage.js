a439f10c6fbe75098dad75781acb3a44184efd29
SusanBird <susanbird391@gmail.com>
2022-05-10 16:02:10 -0700

    created routes and new files

---
+++ src/BestsellersDetailPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function BestsellersDetailPage() {
+  return (
+    <div>BestsellersDetailPage</div>
+  );
+}
---
+++ src/BestsellersListPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function BestsellersListPage() {
+  return (
+    <div>BestsellersListPage</div>
+  );
+}
